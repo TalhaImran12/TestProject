dc5da88ecbefd8f556e728ba7a2269eef895cd35
TalhaImran20 <imrantalha176@gmail.com>
2023-05-06 12:02:17 +0200

    first commit

---
+++ src/test/java/testingAssignment/checkComplexNum.java
@@ -0,0 +1,54 @@
+package testingAssignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class checkComplexNum {
+	
+	public class ComplexCalculationTest {
+	    
+	    WebDriver driver;
+	    
+	    @BeforeTest
+	    public void setup(){
+	        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+	        driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.get("https://www.calculator.net/");
+	    }
+	    
+	    @Test(description = "Verify that the calculator can perform complex calculations correctly.")
+	    public void verifyComplexCalculation() {
+	        
+	        // Inputting the equation "(2+3)*5"
+	        WebElement two = driver.findElement(By.xpath("//span[contains(text(),'2')]"));
+	        two.click();
+	        WebElement plus = driver.findElement(By.xpath("//tbody/tr[2]/td[2]/div[1]/div[1]/span[4]"));
+	        plus.click();
+	        WebElement three = driver.findElement(By.xpath("//span[contains(text(),'3')]"));
+	        three.click();
+	        WebElement multiply = driver.findElement(By.xpath("//span[contains(text(),'×')]"));
+	        multiply.click();
+	        WebElement five = driver.findElement(By.xpath("//span[contains(text(),'5')]"));
+	        five.click();
+	        WebElement equals = driver.findElement(By.xpath("//span[contains(text(),'=')]"));
+	        equals.click();
+	        
+	        // Getting the result and verifying if it is correct
+	        WebElement result = driver.findElement(By.xpath("//div[@id='sciOutPut']"));
+	        Assert.assertEquals(result.getText().trim(), "17".trim());
+	    }
+	    @AfterTest
+	    public void teardown() {
+	        driver.quit();
+	    }
+	}
+
+
+}
